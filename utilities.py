f7bd90ca457dfefed885da0029f29a689fc7a685
Martin Zillmer <zillmermartin@gmail.com>
2023-12-09 23:00:07 +0100

    Add second part of experiment 1

--- utilities.py
+++ utilities.py
@@ -3,6 +3,11 @@ import math
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 import matplotlib.ticker as ticker
+import torch
+import numpy as np
+import torch.random
+import os
+import random
 
 def asMinutes(s):
     m = math.floor(s / 60)
@@ -23,3 +28,15 @@ def showPlot(points):
     loc = ticker.MultipleLocator(base=0.2)
     ax.yaxis.set_major_locator(loc)
     plt.plot(points)
+
+def set_seed(seed: int = 42) -> None:
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    # When running on the CuDNN backend, two further options must be set
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    # Set a fixed value for the hash seed
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    print(f"Random seed set as {seed}")
